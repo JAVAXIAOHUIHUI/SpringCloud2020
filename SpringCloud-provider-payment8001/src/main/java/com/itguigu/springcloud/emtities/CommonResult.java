854277ee20cc5fd362b5e45d5c9d4e0359c721d5
hu <1762322908@qq.com>
2021-04-16 21:57:57 +0800

    '创建JSON封装返回类'

---
+++ SpringCloud-provider-payment8001/src/main/java/com/itguigu/springcloud/emtities/CommonResult.java
@@ -0,0 +1,31 @@
+package com.itguigu.springcloud.emtities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @AUTHOR HU
+ * @DATE 年04月16日21:57
+ * 创建JSON封装返回类
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommonResult<T> implements Serializable {
+
+    private Integer code;
+
+    private String message;
+
+    private T data;
+
+    public CommonResult(Integer code,String message){
+        this.code = code;
+        this.message = message;
+        this.data = null;
+    }
+
+}
