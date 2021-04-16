e51d4869ac8e375995737f11996f0f4ec0dc90a0
hu <1762322908@qq.com>
2021-04-16 22:29:26 +0800

    '创建启动类'

---
+++ SpringCloud-consumer-order80/src/main/java/com/itguigu/springcloud/OrderMain80.java
@@ -0,0 +1,16 @@
+package com.itguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @AUTHOR HU
+ * @DATE 年04月16日22:28
+ */
+@SpringBootApplication
+public class OrderMain80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class);
+    }
+}
