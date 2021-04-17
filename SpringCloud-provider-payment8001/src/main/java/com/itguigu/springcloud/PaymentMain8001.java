257af7f6f1870b4ba47291024d43cd346d86e2fd
hu <1762322908@qq.com>
2021-04-17 21:24:47 +0800

    '修复eureka配置地址错误'

--- SpringCloud-provider-payment8001/src/main/java/com/itguigu/springcloud/PaymentMain8001.java
+++ SpringCloud-provider-payment8001/src/main/java/com/itguigu/springcloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.itguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -9,7 +10,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @DATE 年04月16日21:53
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class);
