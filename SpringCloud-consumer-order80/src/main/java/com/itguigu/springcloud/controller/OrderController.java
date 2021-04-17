26fc5d01322bcfdd80d97b0425faa54b20ec1532
hu <1762322908@qq.com>
2021-04-17 22:31:40 +0800

    '集群配置修改'

--- SpringCloud-consumer-order80/src/main/java/com/itguigu/springcloud/controller/OrderController.java
+++ SpringCloud-consumer-order80/src/main/java/com/itguigu/springcloud/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.itguigu.springcloud.entities.CommonResult;
 import com.itguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ import javax.annotation.Resource;
 @Slf4j
 public class OrderController {
 
-    public static final String PAYMENT_URL = "http://localhost:8001";
+    public static final String PAYMENT_URL = "http://SPRINGCLOUD-PAYMENT-SERVICE";
 
     @Resource
     private RestTemplate restTemplate;
--- SpringCloud-provider-payment8002/src/main/java/com/itguigu/springcloud/controller/PaymentController.java
+++ SpringCloud-provider-payment8002/src/main/java/com/itguigu/springcloud/controller/PaymentController.java
@@ -4,9 +4,11 @@ import com.itguigu.springcloud.entities.CommonResult;
 import com.itguigu.springcloud.entities.Payment;
 import com.itguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+
 
 /**
  * @author hu
@@ -18,15 +20,18 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
-        log.info("*****插入结果："+result);
+        log.info("*****插入结果："+result + serverPort);
 
         if(result > 0) {
-            return new CommonResult(200,"插入数据库成功",result);
+            return new CommonResult(200,"插入数据库成功,ip ：" + serverPort,result);
         }else{
-            return new CommonResult(444,"插入数据库失败",null);
+            return new CommonResult(444,"插入数据库失败,ip ：" + serverPort,null);
         }
     }
 
@@ -35,9 +40,9 @@ public class PaymentController {
         Payment payment = paymentService.getPaymentById(id);
 
         if(payment != null) {
-            return new CommonResult(200,"查询成功",payment);
+            return new CommonResult(200,"查询成功,ip ：" + serverPort,payment);
         }else{
-            return new CommonResult(444,"没有对应记录,查询ID: "+id,null);
+            return new CommonResult(444,"没有对应记录,查询ID: "+id +serverPort,null);
         }
     }
 }
