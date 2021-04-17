4af491c6ddd7cfe27c11b0b1417b1b1680d19f97
hu <1762322908@qq.com>
2021-04-17 22:44:30 +0800

    '添加yml文件和controller'

---
+++ SpringCloud-ZK-provider-payment8004/src/main/java/com/itguigu/springcloud/controller/ZkPaymentController.java
@@ -0,0 +1,26 @@
+package com.itguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @AUTHOR HU
+ * @DATE 年04月17日22:42
+ */
+@Slf4j
+@RestController
+public class ZkPaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "payment/zk")
+    public String paymentZk() {
+        return "SpringCloud with zookeeper:" + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+
+}
