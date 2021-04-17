0e434fc74342817f064159e9135d3a7bfb967017
hu <1762322908@qq.com>
2021-04-17 18:18:47 +0800

    '修改eureka客户端启动类'配置'

--- SpringCloud-consumer-order80/src/main/java/com/itguigu/springcloud/OrderMain80.java
+++ SpringCloud-consumer-order80/src/main/java/com/itguigu/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.itguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @AUTHOR HU
  * @DATE 年04月16日22:28
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
 
     public static void main(String[] args) {
--- SpringCloud-eureka-server7002/src/main/java/com/itguigu/springcloud/EurekaMain7002.java
+++ SpringCloud-eureka-server7002/src/main/java/com/itguigu/springcloud/EurekaMain7002.java
@@ -2,16 +2,16 @@ package com.itguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @AUTHOR HU
- * @DATE 年04月16日21:53
+ * @DATE 年04月17日18:03
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7002 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class);
+        SpringApplication.run(EurekaMain7002.class);
     }
 }
