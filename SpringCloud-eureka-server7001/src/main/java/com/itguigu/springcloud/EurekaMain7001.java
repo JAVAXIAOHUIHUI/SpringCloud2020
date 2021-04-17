bc1ec83c38a6af668887f37f93355354bf3354a5
hu <1762322908@qq.com>
2021-04-17 18:08:13 +0800

    '修复注解@EnableEurekaServer'

---
+++ SpringCloud-eureka-server7001/src/main/java/com/itguigu/springcloud/EurekaMain7001.java
@@ -0,0 +1,18 @@
+package com.itguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @AUTHOR HU
+ * @DATE 年04月17日18:03
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class);
+    }
+}
