32aa04c7ef4b2602533e9908d0565055c4cd7b93
hu <1762322908@qq.com>
2021-04-18 18:08:26 +0800

    '创建controller'

---
+++ SpringCloud-provider-Consul-payment8006/src/main/java/com/itguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.itguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @AUTHOR HU
+ * @DATE 年04月18日18:07
+ */
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "payment/consul")
+    public String paymentConsul() {
+        return "SpringCloud with consul:" + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+}
+
