8abdc415a78e109578435d4c828b5836f5976fec
hu <1762322908@qq.com>
2021-04-18 22:58:24 +0800

    '创建yml配置文件以及启动类'

---
+++ SpringCloud-provider-Hystrix-payment8001/src/main/java/com/itguigu/springcloud/PaymentHystrixMain8001.java
@@ -0,0 +1,17 @@
+package com.itguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @AUTHOR HU
+ * @DATE 年04月18日22:57
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentHystrixMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentHystrixMain8001.class, args);
+    }
+}
