a8fb0d26228ac63b08d080385bdb02ff494652c1
hu <1762322908@qq.com>
2021-04-18 20:39:30 +0800

    '修改80Ribbon随即配置'

--- SpringCloud-consumer-order80/src/main/java/com/itguigu/springcloud/config/ApplicationContextConfig.java
+++ SpringCloud-consumer-order80/src/main/java/com/itguigu/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,7 @@
 package com.itguigu.springcloud.config;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,5 +18,10 @@ public class ApplicationContextConfig {
     @LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public IRule iRule(){
+        return new RandomRule();
     }
 }
