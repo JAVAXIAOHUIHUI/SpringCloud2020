19dece2fb76e36aa239db2774f93eb5ad528f824
hu <1762322908@qq.com>
2021-04-19 21:34:51 +0800

    '添加service和controller层'

---
+++ SpringCloud-consumer-Feign-Hystrix-order80/src/main/java/com/itguigu/springcloud/controller/OrderHystrixController.java
@@ -0,0 +1,42 @@
+package com.itguigu.springcloud.controller;
+
+import com.itguigu.springcloud.service.HystrixService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @AUTHOR HU
+ * @DATE 年04月19日21:22
+ */
+@RestController
+@Slf4j
+public class OrderHystrixController {
+
+    @Resource
+    private HystrixService hystrixService;
+
+    @GetMapping("/order/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
+        String result = hystrixService.paymentInfo_OK(id);
+        log.info("order*****result:" + result);
+        return result;
+    }
+
+    /**
+     * 超时访问
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/order/hystrix/timeout/{id}")
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+        String result = hystrixService.paymentInfo_TimeOut(id);
+        log.info("order*****result:" + result);
+        return result;
+
+    }
+}
---
+++ SpringCloud-consumer-Feign-Hystrix-order80/src/main/java/com/itguigu/springcloud/service/HystrixService.java
@@ -0,0 +1,34 @@
+package com.itguigu.springcloud.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @AUTHOR HU
+ * @DATE 年04月19日21:20
+ */
+@FeignClient(value = "cloud-provider-hystrix-payment")
+public interface HystrixService {
+
+
+    /**
+     * 正常访问
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/payment/hystrix/ok/{id}")
+    String paymentInfo_OK(@PathVariable("id") Integer id);
+
+    /**
+     * 超时访问
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    String paymentInfo_TimeOut(@PathVariable("id") Integer id);
+
+
+}
