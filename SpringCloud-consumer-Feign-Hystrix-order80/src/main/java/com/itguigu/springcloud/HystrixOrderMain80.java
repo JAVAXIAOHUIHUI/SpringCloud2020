3964f5c6c6618222dc94eb6545015e471b5d775b
hu <1762322908@qq.com>
2021-04-19 21:19:39 +0800

    '添加yml配置文件以及启动类'

---
+++ SpringCloud-consumer-Feign-Hystrix-order80/src/main/java/com/itguigu/springcloud/HystrixOrderMain80.java
@@ -0,0 +1,19 @@
+package com.itguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @AUTHOR HU
+ * @DATE 年04月19日21:18
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class HystrixOrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixOrderMain80.class);
+    }
+}
