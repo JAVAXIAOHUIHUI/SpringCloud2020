322814227f7199748faa3e892650daf5c7e59513
hu <1762322908@qq.com>
2021-04-19 21:44:07 +0800

    '配置高压下熔断策略'

--- SpringCloud-consumer-Feign-Hystrix-order80/src/main/java/com/itguigu/springcloud/controller/OrderHystrixController.java
+++ SpringCloud-consumer-Feign-Hystrix-order80/src/main/java/com/itguigu/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,7 @@
 package com.itguigu.springcloud.controller;
 
 import com.itguigu.springcloud.service.HystrixService;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +33,7 @@ public class OrderHystrixController {
      * @param id
      * @return
      */
+
     @GetMapping("/order/hystrix/timeout/{id}")
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         String result = hystrixService.paymentInfo_TimeOut(id);
--- SpringCloud-provider-Hystrix-payment8001/src/main/java/com/itguigu/springcloud/controller/PaymentHystrixController.java
+++ SpringCloud-provider-Hystrix-payment8001/src/main/java/com/itguigu/springcloud/controller/PaymentHystrixController.java
@@ -1,6 +1,10 @@
 package com.itguigu.springcloud.controller;
 
+import ch.qos.logback.core.util.TimeUtil;
 import com.itguigu.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import io.micrometer.core.instrument.util.TimeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @AUTHOR HU
@@ -17,39 +22,51 @@ import javax.annotation.Resource;
 @Slf4j
 public class PaymentHystrixController {
 
+    @Resource
+    private PaymentHystrixService paymentHystrixService;
 
-        @Resource
-        private PaymentHystrixService paymentHystrixService;
+    @Value("${server.port}")
+    private String servicePort;
 
-        @Value("${server.port}")
-        private String servicePort;
+    /**
+     * 正常访问
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
+        String result = paymentHystrixService.paymentInfo_OK(id);
+        log.info("*****result:" + result);
+        return result;
+    }
 
-        /**
-         * 正常访问
-         *
-         * @param id
-         * @return
-         */
-        @GetMapping("/payment/hystrix/ok/{id}")
-        public String paymentInfo_OK(@PathVariable("id") Integer id) {
-            String result = paymentHystrixService.paymentInfo_OK(id);
+    /**
+     * 超时访问
+     *
+     * @param id
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "payment_TimeOutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+        int second = 5;
+        String result = null;
+        try {
+            TimeUnit.SECONDS.sleep(second);
+            result = paymentHystrixService.paymentInfo_TimeOut(id);
             log.info("*****result:" + result);
-            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return result;
 
-        /**
-         * 超时访问
-         *
-         * @param id
-         * @return
-         */
-        @GetMapping("/payment/hystrix/timeout/{id}")
-        public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
-            String result = paymentHystrixService.paymentInfo_TimeOut(id);
-            log.info("*****result:" + result);
-            return result;
+    }
 
-        }
-
+    public String payment_TimeOutHandler(Integer id){
+        return "调用支付接口超时异常：\t " + "\t当前线程池名称" + Thread.currentThread().getName();
+    }
 
 }
